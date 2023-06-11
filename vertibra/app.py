890181e33b18821cefad80f6b287066c83e05e35
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-06-11 16:20:00 +0000

    half api

--- vertibra/app.py
+++ vertibra/app.py
@@ -10,8 +10,8 @@ app = FastAPI()
 # Function to get arguments
 def get_args(weight_path, data_path):
     args = argparse.Namespace()
-    args.resume = weight_path
-    args.data_dir = data_path
+    args.resume = model_last.pth
+    args.data_dir = input
     args.dataset = 'spinal'
     args.phase = 'test'
     # Add other arguments with default values here as in your main.py
@@ -31,6 +31,12 @@ async def predict(item: Item):
     is_object.eval(args, save=False)
     # You may want to retrieve and return some results here, 
     # Currently, your eval method does not seem to return anything
+
+    time.sleep(2)
+    print("wait 2 sec")
+
+    is_object = test_e.Network1(args)
+    is_object.test(args, save=False)
 
 if __name__ == "__main__":
     import uvicorn
